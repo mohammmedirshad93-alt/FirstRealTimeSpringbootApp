891f70120fa4fdb0db0a047d7615dcbb65ec6b80
mohd irshad <you@example.com>
2025-09-04 22:29:26 +0530

    itsrun

---
+++ src/main/java/com/excelr/FirstRealTimeSpringbootAppApplication.java
@@ -0,0 +1,13 @@
+package com.excelr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstRealTimeSpringbootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstRealTimeSpringbootAppApplication.class, args);
+	}
+
+}
