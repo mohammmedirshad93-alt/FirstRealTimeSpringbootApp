5b6818218aef16fbb005eae9530a60d5c8521765
mohd irshad <you@example.com>
2025-09-04 21:38:06 +0530

    JavaRealTimeProjectadded

---
+++ Documents/workspace-spring-tools-for-eclipse-4.31.0.RELEASE/JavaRealTimeProject/src/main/java/com/excelr/JavaRealTimeProjectApplication.java
@@ -0,0 +1,15 @@
+package com.excelr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRealTimeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRealTimeProjectApplication.class, args);
+		
+		System.out.println("welcome");
+	}
+
+}
